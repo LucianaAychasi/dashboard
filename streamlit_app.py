9247e89727f25db191524afe90a1831718d155f6
LucianaAychasi <136522330+LucianaAychasi@users.noreply.github.com>
2023-06-23 23:11:01 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.subheader("Dashboard sobre la calidad de aire en la municipalidad de Miraflor
 mes = ['Julio 2020', 'Agosto 2020', 'Septiembre 2020', 'Octubre 2020', 'Noviembre 2020', 'Diciembre 2020', 'Enero 2021', 'Febrero 2021', 'Marzo 2021', 'Abril 2021', 'Mayo 2021', 'Junio 2021', ]
 mes_mapeado = {i: mes[i-1] for i in range(1, 13)}
 selectbox_options = [mes_mapeado[i] for i in range(1, 13)]
-for i, url in enumerate(urls):
+for j, url in enumerate(urls):
     dfs[url] = pd.read_excel(url)
     df = dfs[url]
     df['Fecha'] = pd.to_datetime(df['Fecha'])
