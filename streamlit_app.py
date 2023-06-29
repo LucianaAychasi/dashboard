8adba78658e87d8c0a5cbf4e363f3a3d37017ccc
LucianaAychasi <luciana.aychasi@upch.pe>
2023-06-29 00:24:55 -0500

    Se configuro el sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,110 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 import plotly.express as px
-url = "https://www.datosabiertos.gob.pe/sites/default/files/13_Monitoreo_Junio_2021.xlsx"
-df = pd.read_excel(url)
-st.set_page_config(page_title="Dashboard", layout='centered', initial_sidebar_state='collapsed')
-st.subheader("Dashboard sobre la calidad de aire en la municipalidad de Miraflores - 2021")
-df['Fecha'] = pd.to_datetime(df['Fecha'])
-df['Month'] = df['Fecha'].dt.month
-mes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-mes_mapeado = {i: mes[i-1] for i in range(1, 13)}
-selectbox_options = [mes_mapeado[i] for i in range(1, 13)]
-mes_seleccionado = st.selectbox("Seleccione mes", selectbox_options)
-mes_data = list(mes_mapeado.keys())[list(mes_mapeado.values()).index(mes_seleccionado)]
-filtro_df = df[df['Month'] == mes_data]
-st.markdown('### Humedad ambiente')
-fig = px.line(filtro_df, x="Fecha", y="Humedad (%)", title='Humedad (%)', markers=True, text='Humedad (%)')
-fig.update_traces(line_color='#98EECC')
-st.plotly_chart(fig, use_container_width=True)
+st.set_page_config(page_title="Dashboard", layout='wide',
+                   initial_sidebar_state='auto')
+def procesar_archivo_excel(archivo):
+    dfs = []
+    for sheet_name in archivo.sheet_names:
+        df = pd.read_excel(archivo, sheet_name=sheet_name)
+        columnas_interes = ["Fecha", "CO (ug/m3)", "H2S (ug/m3)", "NO2 (ug/m3)", "O3 (ug/m3)",
+                            "PM10 (ug/m3)", "PM2.5 (ug/m3)", "SO2 (ug/m3)", "Ruido (dB)",
+                            "UV", "Humedad (%)", "Presion (Pa)", "Temperatura (C)"]
+        df = df[columnas_interes]
+        df = df.replace('-', np.nan)
+        df = df.fillna(0)
+        dfs.append(df)
+    return dfs
+def main():
+    anio = st.sidebar.radio("Seleccione el año", options=[2020, 2021])
+    meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+         'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    mes_seleccionado = st.sidebar.selectbox("Seleccione el mes", options=meses)
+    archivos = []
+    ubicaciones = []
+    if anio == 2020:
+        if mes_seleccionado.lower() == "julio":
+            archivo = pd.ExcelFile("Monitoreo_julio.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "agosto":
+            archivo = pd.ExcelFile("Monitoreo_agosto.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "setiembre":
+            archivo1 = pd.ExcelFile("Monitoreo_setiembre_Bonilla.xlsx")
+            archivo2 = pd.ExcelFile("Monitoreo_setiembre_Ov.Miraflores.xlsx")
+            archivos.append(archivo1)
+            archivos.append(archivo2)
+            ubicaciones.append(
+                "Complejo Deportivo Manuel Bonilla")
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "octubre":
+            archivo = pd.ExcelFile("Monitoreo_octubre.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "noviembre":
+            archivo = pd.ExcelFile("6_Monitoreo_Noviembre.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "diciembre":
+            archivo = pd.ExcelFile("7_Monitoreo_Diciembre.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+    elif anio == 2021:
+        if mes_seleccionado.lower() == "enero":
+            archivo = pd.ExcelFile("8_Monitoreo_Enero_2021.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "febrero":
+            archivo = pd.ExcelFile("9_Monitoreo_Febrero_2021.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "marzo":
+            archivo = pd.ExcelFile("10_Monitoreo_Marzo_2021.xlsx")
+            archivos.append(archivo)
+            ubicacionesi.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "abril":
+            archivo = pd.ExcelFile("11_Monitoreo_Abril_2021.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "mayo":
+            archivo = pd.ExcelFile("12_Monitoreo_Mayo_2021.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
+        elif mes_seleccionado.lower() == "junio":
+            archivo = pd.ExcelFile("13_Monitoreo_Junio_2021.xlsx")
+            archivos.append(archivo)
+            ubicaciones.append("Óvalo de Miraflores")
 
-st.markdown(f'### Temperatura (C)')
-fig = px.line(filtro_df, x="Fecha", y="Temperatura (C)", title="Temperatura (C)", markers=True, text="Temperatura (C)")
-fig.update_traces(line_color='#FFC93C')
-st.plotly_chart(fig, use_container_width=True)
+    if archivos:
+        st.subheader(
+            f"Mediciones de Calidad de Aire - {mes_seleccionado.capitalize()} {anio}")
+        for archivo, ubicacion in zip(archivos, ubicaciones):
+            dfs = procesar_archivo_excel(archivo)
+            if dfs:
+                for i, df in enumerate(dfs):
+                    if len(archivo.sheet_names) > 1:
+                        if i == 0:
+                            st.markdown(
+                                "📍 Complejo Deportivo Manuel Bonilla")
+                        else:
+                            st.markdown(
+                                "📍 Óvalo de Miraflores")
+                    else:
+                        st.markdown(f"📍 {ubicacion}")
 
-st.markdown('### Monoxido de carbono')
-fig = px.line(filtro_df, x="Fecha", y="CO (ug/m3)", title='Monoxido de carbono', markers=True, text='CO (ug/m3)')
-fig.update_traces(line_color='#9DB2BF')
-st.plotly_chart(fig, use_container_width=True)
+                    for columna in df.columns[1:]:
+                        st.subheader(columna)
+                        fig = px.line(df, x="Fecha", y=columna)
+                        st.plotly_chart(fig)
+                    st.snow()
+            else:
+                st.warning(
+                    "Los archivos seleccionados no contienen datos válidos.")
+    else:
+        st.warning("No se encontró archivo para el año y mes seleccionado.")
 
-st.markdown('### Ácido sulfhídrico')
-fig = px.line(filtro_df, x="Fecha", y="H2S (ug/m3)", title='Ácido sulfhídrico', markers=True, text='H2S (ug/m3)')
-fig.update_traces(line_color='#EEE2DE')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Dióxido de nitrógeno')
-fig = px.line(filtro_df, x="Fecha", y="NO2 (ug/m3)", title='Dióxido de nitrógeno', markers=True, text='NO2 (ug/m3)')
-fig.update_traces(line_color='#00ABB3')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Ozono')
-fig = px.line(filtro_df, x="Fecha", y="O3 (ug/m3)", title='Ozono', markers=True, text='O3 (ug/m3)')
-fig.update_traces(line_color='#39A2DB')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Partículas de menos de 10 micrómetros')
-fig = px.line(filtro_df, x="Fecha", y="PM10 \n(ug/m3)", title='PM10', markers=True, text='PM10 \n(ug/m3)')
-fig.update_traces(line_color='#A7BBC7')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Materia particulada 2.5')
-fig = px.line(filtro_df, x="Fecha", y="PM2.5 \n(ug/m3)", title='PM2.5', markers=True, text='PM2.5 \n(ug/m3)')
-fig.update_traces(line_color='#BDD2B6')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Dióxido de azufre')
-fig = px.line(filtro_df, x="Fecha", y="SO2 (ug/m3)", title='Dióxido de azufre', markers=True, text='SO2 (ug/m3)')
-fig.update_traces(line_color='#BDD2B6')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Ruido ambiente')
-fig = px.line(filtro_df, x="Fecha", y="Ruido (dB)", title='Ruido', markers=True, text='Ruido (dB)')
-fig.update_traces(line_color='#B799FF')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Rayos ultravioleta')
-fig = px.line(filtro_df, x="Fecha", y="UV", title='Rayos ultravioleta', markers=True, text='UV')
-fig.update_traces(line_color='#FF6969')
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown('### Presión atmosférica')
-fig = px.line(filtro_df, x="Fecha", y="Presion \n(Pa)", title='Presión atmosférica', markers=True, text='Presion \n(Pa)')
-fig.update_traces(line_color='#A555EC')
-st.plotly_chart(fig, use_container_width=True)
+if __name__ == "__main__":
+    main()
