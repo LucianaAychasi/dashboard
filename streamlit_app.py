464d4dfc4bef09062c17c3d23eb466c8c649f246
Carmen Poma <122483546+carmenpoma163@users.noreply.github.com>
2023-06-29 21:34:48 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def main():
             archivos.append(archivo)
             ubicaciones.append("Óvalo de Miraflores")
         elif mes_seleccionado.lower()=="noviembre":
-            archivo = pd.ExcelFile("6_Monitoreo_Noviembre.xlsx")
+            archivo=pd.ExcelFile("6_Monitoreo_Noviembre.xlsx")
             archivos.append(archivo)
             ubicaciones.append("Óvalo de Miraflores")
         elif mes_seleccionado.lower() == "diciembre":
